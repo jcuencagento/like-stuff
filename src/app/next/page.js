f4398556680706bee747669af6ca4aa4004f22c5
Javi Cuenca <jcuencagento@gmail.com>
2024-06-13 23:54:45 +0200

    New page

--- src/app/next/page.js
+++ src/app/next/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Next() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -42,11 +42,11 @@ export default function Home() {
 
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                 <Link
-                href="/next"
+                href="/"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 >
                 <h2 className={`mb-3 text-2xl font-semibold`}>
-                    Next{" "}
+                    Back{" "}
                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none"> -&gt; </span>
                 </h2>
                 </Link>
